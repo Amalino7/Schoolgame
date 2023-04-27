ee992572ad5a8f72786b9ab26aaa9f5fbc027849
Amalino7 <101348001+Amalino7@users.noreply.github.com>
2023-04-27 14:05:35 +0300

    Tiled map added
    Added all the assets (for now) and super basic map.

---
+++ TIledproject/tilesets/Items.tsx
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Items" tilewidth="128" tileheight="128" tilecount="32" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="Pureassets/coinBronze.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="Pureassets/coinGold.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="Pureassets/coinGold_ll.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="Pureassets/coinGold_lr.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="Pureassets/coinGold_ul.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="Pureassets/coinGold_ur.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="Pureassets/coinSilver.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="Pureassets/coinSilver_test.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="Pureassets/flagGreen_down.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="Pureassets/flagGreen1.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="Pureassets/flagGreen2.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="Pureassets/flagRed_down.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="Pureassets/flagRed1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="Pureassets/flagRed2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="Pureassets/flagYellow_down.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="Pureassets/flagYellow1.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="Pureassets/flagYellow2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="Pureassets/gemBlue.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="Pureassets/gemGreen.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="Pureassets/gemRed.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="Pureassets/gemYellow.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="Pureassets/gold_1.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="Pureassets/gold_2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="Pureassets/gold_3.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="Pureassets/gold_4.png"/>
+ </tile>
+ <tile id="25">
+  <image width="128" height="128" source="Pureassets/keyBlue.png"/>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="Pureassets/keyGreen.png"/>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="Pureassets/keyRed.png"/>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="Pureassets/keyYellow.png"/>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="Pureassets/ladderMid.png"/>
+ </tile>
+ <tile id="30">
+  <image width="128" height="128" source="Pureassets/ladderTop.png"/>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="Pureassets/star.png"/>
+ </tile>
+</tileset>
