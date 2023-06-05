04028184ed7408106569840d4b1ac75c7b45d6e8
PavkataXD74 <pavel1sou@abv.bg>
2023-06-05 10:01:07 +0300

    fixed path problems

--- new_assets/tilesets/interactables.tsx
+++ new_assets/tilesets/interactables.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="interactables" tilewidth="128" tileheight="128" tilecount="324" columns="18">
- <image source="../../../../../Pictures/clumsyM_assets/tilesets/png_files/interactables.png" width="2304" height="2304"/>
+ <image source="interactables.png" width="2304" height="2304"/>
 </tileset>
