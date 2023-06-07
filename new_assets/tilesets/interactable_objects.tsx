016a8c674f876cbcf05034941253a999cf653117
PavkataXD74 <pavel1sou@abv.bg>
2023-06-07 15:45:46 +0300

    changed sprites that didn't work
    created a new image collection based tile set that contains full images instead of sprites that where separated on 4 different tiles

---
+++ new_assets/tilesets/interactable_objects.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="interactable_objects" tilewidth="256" tileheight="256" tilecount="20" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="../image_collection/box.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="../image_collection/button_S1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="../image_collection/button_S2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="../image_collection/colector_1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="256" source="../image_collection/colector_2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="256" source="../image_collection/colector_3.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="256" source="../image_collection/colector_4.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="256" source="../image_collection/emiter_1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="256" source="../image_collection/emiter_2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="256" height="256" source="../image_collection/emiter_3.png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="256" source="../image_collection/emiter_4.png"/>
+ </tile>
+ <tile id="11">
+  <image width="256" height="256" source="../image_collection/finishline.png"/>
+ </tile>
+ <tile id="12">
+  <image width="256" height="256" source="../image_collection/ref_static_1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="256" height="256" source="../image_collection/ref_static_2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="256" height="256" source="../image_collection/ref_static_3.png"/>
+ </tile>
+ <tile id="15">
+  <image width="256" height="256" source="../image_collection/ref_static_4.png"/>
+ </tile>
+ <tile id="16">
+  <image width="256" height="256" source="../image_collection/reflector_1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="256" height="256" source="../image_collection/reflector_2.png"/>
+ </tile>
+ <tile id="18">
+  <image width="256" height="256" source="../image_collection/reflector_3.png"/>
+ </tile>
+ <tile id="19">
+  <image width="256" height="256" source="../image_collection/reflector_4.png"/>
+ </tile>
+</tileset>
